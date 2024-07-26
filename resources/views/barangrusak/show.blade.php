8e5eb7358204369e3b92ab7de6a1e305556ed73b
FirdausMoh <mohamadfahri302@gmail.com>
2024-07-26 14:44:23 +0700

    tesst

--- resources/views/barangrusak/show.blade.php
+++ resources/views/barangrusak/show.blade.php
@@ -9,28 +9,43 @@
 
             <div class="container mt-4">
                 <div class="card shadow">
-                    <div class="card-header bg-primary text-white">
+                    <div class="card-header text-white text-center" style="background-color: rgb(1, 1, 95);">
                         <h3 class="mb-0">Detail Barang Rusak</h3>
                     </div>
                     <div class="card-body">
+                        <div class="mb-4">
+                            <h2 class="font-weight-bold">{{ $barangrusak->Nama_Barang }}</h2>
+                            <h4 class="text-muted">{{ $barangrusak->Kategori_Barang }}</h4>
+                            <hr>
+                        </div>
                         <div class="row">
-                            <div class="col-md-6">
-                                <h4 class="card-title">{{ $barangrusak->nama }}</h4>
-                                
-                                <hr>
-                                <p><strong>Merk:</strong> {{ $barangrusak->merk }}</p>
-                                <p><strong>No. Seri:</strong> {{ $barangrusak->no_seri }}</p>
-                                <p><strong>Tipe:</strong> {{ $barangrusak->tipe }}</p>
-                                <p><strong>Keterangan:</strong> {{ $barangrusak->keterangan }}</p>
+                            <div class="col-md-5 d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('images/' . $barangrusak->Gambar) }}" alt="{{ $barangrusak->Nama_Barang }}" class="img-fluid rounded" style="width: 300px;">
                             </div>
-                            <div class="col-md-6 d-flex justify-content-center align-items-center">
-                                <img src="{{ Vite::asset('resources/assets/' . $barangrusak->image) }}" alt="{{ $barangrusak->nama }}" class="img-fluid rounded">
+                            <div class="col-md-7 mt-4 ">
+                                <div class="mb-3">
+                                    <h5 class="font-weight-bold">Pihak Pemohon: {{ $barangrusak->Pihak_Pemohon }}</h5>
+                                </div>
+                                <hr>
+                                <div class="mb-3">
+                                    <h5 class="font-weight-bold">Jumlah Barang: {{ $barangrusak->Jumlah_Barang }} </h5>
+                                </div>
+                                <hr>
+                                <div class="mb-3">
+                                    <h5 class="font-weight-bold">Tanggal Pengembalian: {{ $barangrusak->Tanggal_Retur }}</h5>
+                                </div>
+                                <hr>
+                                <div class="mb-3">
+                                    <h5 class="font-weight-bold">Deskripsi: {{ $barangrusak->Deskripsi }}</h5>
+                                </div>
+                                <hr>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="card-footer text-end">
                         <a href="{{ route('barangrusak.index') }}" class="btn btn-secondary">
-                            Close
+                            Back
                         </a>
                     </div>
                 </div>
--- resources/views/retur/create.blade.php
+++ resources/views/retur/create.blade.php
@@ -6,97 +6,105 @@
         <div id="content">
             @include('layouts.navbar')
             <div class="container-fluid">
-                <h1 class="h3 mb-4 text-gray-800">Create Retur Barang</h1>
+                <div class="d-sm-flex align-items-center justify-content-center rounded-top mb-0"
+                    style="background-color: rgb(1, 1, 95);">
+                    <h1 class="h3 mb-0 text-white"><strong>Form Retur Barang</strong></h1>
+                </div>
 
                 <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Form Retur Barang</h6>
-                    </div>
+
                     <div class="card-body">
                         <form action="{{ route('retur.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
 
-                            <div class="mb-3">
-                                <label for="Id_Barang_Keluar" class="form-label">ID Barang Keluar</label>
-                                <input type="text" class="form-control" id="Id_Barang_Keluar" name="Id_Barang_Keluar"
-                                    value="{{ old('Id_Barang_Keluar', request()->query('id')) }}" readonly>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="Id_User" class="form-label">ID User</label>
+                                    <input type="text" class="form-control" id="Id_User" name="Id_User"
+                                        value="{{ Auth::user()->id }}" readonly>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Pihak_Pemohon" class="form-label">Pihak Pemohon</label>
+                                    <input type="text" class="form-control" id="Pihak_Pemohon" name="Pihak_Pemohon"
+                                        value="{{ old('Pihak_Pemohon', request()->query('pihak_peminjam')) }}" readonly>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Id_Barang_Keluar" class="form-label">ID Barang Keluar</label>
+                                    <input type="text" class="form-control" id="Id_Barang_Keluar" name="Id_Barang_Keluar"
+                                        value="{{ old('Id_Barang_Keluar', request()->query('id')) }}" readonly>
+                                </div>
+
+                                <div class="col-md-6 mb-3">
+                                    <label for="Kode_Barang" class="form-label">Kode Barang</label>
+                                    <input type="text" class="form-control" id="Kode_Barang" name="Kode_Barang"
+                                        value="{{ old('Kode_Barang', request()->query('kode_barang')) }}" readonly>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="Id_User" class="form-label">ID User</label>
-                                <input type="text" class="form-control" id="Id_User" name="Id_User"
-                                    value="{{ Auth::user()->id }}" readonly>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="Kategori_Barang" class="form-label">Kategori Barang</label>
+                                    <input type="text" class="form-control" id="Kategori_Barang" name="Kategori_Barang"
+                                        value="{{ old('Kategori_Barang', request()->query('kategori_barang')) }}" readonly>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Nama_Barang" class="form-label">Nama Barang</label>
+                                    <input type="text" class="form-control" id="Nama_Barang" name="Nama_Barang"
+                                        value="{{ old('Nama_Barang', request()->query('nama_barang')) }}" readonly>
+                                </div>
                             </div>
-
-                            <div class="mb-3">
-                                <label for="Id_Status_Retur" class="form-label">Status Retur</label>
-                                <select class="form-control" id="Id_Status_Retur" name="Id_Status_Retur">
-                                    @foreach ($statusRetur as $status)
-                                        <option value="{{ $status->id }}">{{ $status->Nama_Status }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="Tanggal_Retur" class="form-label">Tanggal Retur</label>
+                                    <input type="date" class="form-control" id="Tanggal_Retur" name="Tanggal_Retur"
+                                        value="{{ old('Tanggal_Retur', date('Y-m-d')) }}">
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Jumlah_Barang" class="form-label">Jumlah Barang</label>
+                                    <input type="number" class="form-control" id="Jumlah_Barang" name="Jumlah_Barang"
+                                        value="{{ old('Jumlah_Barang') }}">
+                                </div>
                             </div>
-
-                            <div class="mb-3">
-                                <label for="Id_Jenis_Retur" class="form-label">Jenis Retur</label>
-                                <select class="form-control" id="Id_Jenis_Retur" name="Id_Jenis_Retur">
-                                    @foreach ($jenisRetur as $jenis)
-                                        <option value="{{ $jenis->id }}">{{ $jenis->Jenis_Retur }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="Id_Jenis_Retur" class="form-label">Jenis Retur</label>
+                                    <select class="form-control" id="Id_Jenis_Retur" name="Id_Jenis_Retur">
+                                        @foreach ($jenisRetur as $jenis)
+                                            <option value="{{ $jenis->id }}">{{ $jenis->Jenis_Retur }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Gambar" class="form-label">Upload Gambar</label>
+                                    <input type="file" class="form-control" id="Gambar" name="Gambar">
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label for="Id_Status_Retur" class="form-label">Status Retur</label>
+                                    <select class="form-control" id="Id_Status_Retur" name="Id_Status_Retur">
+                                        @foreach ($statusRetur as $status)
+                                            <option value="{{ $status->id }}">{{ $status->Nama_Status }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-
-                            <div class="mb-3">
-                                <label for="Pihak_Pemohon" class="form-label">Pihak Pemohon</label>
-                                <input type="text" class="form-control" id="Pihak_Pemohon" name="Pihak_Pemohon"
-                                    value="{{ old('Pihak_Pemohon', request()->query('pihak_peminjam')) }}" readonly>
+                            
+                            <div class="row">
+                                <div class="col-md-12 mb-3">
+                                    <label for="Deskripsi" class="form-label">Deskripsi</label>
+                                    <textarea class="form-control" id="Deskripsi" name="Deskripsi" rows="3">{{ old('Deskripsi') }}</textarea>
+                                </div>
                             </div>
-
-                            <div class="mb-3">
-                                <label for="Nama_Barang" class="form-label">Nama Barang</label>
-                                <input type="text" class="form-control" id="Nama_Barang" name="Nama_Barang"
-                                    value="{{ old('Nama_Barang', request()->query('nama_barang')) }}" readonly>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="Kode_Barang" class="form-label">Kode Barang</label>
-                                <input type="text" class="form-control" id="Kode_Barang" name="Kode_Barang"
-                                    value="{{ old('Kode_Barang', request()->query('kode_barang')) }}" readonly>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="Kategori_Barang" class="form-label">Kategori Barang</label>
-                                <input type="text" class="form-control" id="Kategori_Barang" name="Kategori_Barang"
-                                    value="{{ old('Kategori_Barang', request()->query('kategori_barang')) }}" readonly>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="Jumlah_Barang" class="form-label">Jumlah Barang</label>
-                                <input type="number" class="form-control" id="Jumlah_Barang" name="Jumlah_Barang"
-                                    value="{{ old('Jumlah_Barang') }}">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="Deskripsi" class="form-label">Deskripsi</label>
-                                <textarea class="form-control" id="Deskripsi" name="Deskripsi" rows="3">{{ old('Deskripsi') }}</textarea>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="Tanggal_Retur" class="form-label">Tanggal Retur</label>
-                                <input type="date" class="form-control" id="Tanggal_Retur" name="Tanggal_Retur"
-                                    value="{{ old('Tanggal_Retur', date('Y-m-d')) }}">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="gambar" class="form-label">Upload Gambar</label>
-                                <input type="file" class="form-control" id="Gambar" name="Gambar">
-                            </div>
-
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
                     </div>
+                    <div class="row ">
+                        <div class="col-md-12 mb-3 ml-3">
+                            <a href="{{ route('barangkeluar.index') }}" class="btn btn-secondary me-2">Cancel</a>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
